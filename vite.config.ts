e16883f1f6a6d7ece464e4e986ec58326c01e1ed
Masoud Soroush <masoud.s.geo@gmail.com>
2024-05-20 17:05:32 +0200

    feat: add test vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svelte(),
+    svelteTesting({
+      // disable auto cleanup
+      autoCleanup: false,
+      // disable browser resolution condition
+      resolveBrowser: false,
+    }),
+  ],
 })
