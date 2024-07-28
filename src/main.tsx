8f30a082b1877cb38c19ef5c97396c03b492186a
Masoud Soroush <masoud.s.geo@gmail.com>
2024-07-28 16:29:24 +0200

    chore: run msw on production

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import App from './App.tsx'
 import { renderWithProvider } from './renderWithProvider.tsx'
 import './index.css'
 
-if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_MSW) {
-  import('./service/mocks/runtime')
-    .then(({ worker }) => {
-      console.log('development', worker)
-      return worker.start({ onUnhandledRequest: 'bypass' })
-    })
-    .catch((error: Error) => console.log('msw worker error', error))
-}
+// if (process.env.NODE_ENV === 'development' || process.env.REACT_APP_MSW) {
+import('./service/mocks/runtime')
+  .then(({ worker }) => {
+    console.log('development', worker)
+    return worker.start({ onUnhandledRequest: 'bypass' })
+  })
+  .catch((error: Error) => console.log('msw worker error', error))
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   renderWithProvider(App),
