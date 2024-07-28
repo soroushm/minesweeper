203e4d548d64f15dbf91a6fe597e748973edb71a
Masoud Soroush <masoud.s.geo@gmail.com>
2024-07-28 03:29:04 +0200

    refactor: extract MSW handlers

--- src/service/mocks/handlers.ts
+++ src/service/mocks/handlers.ts
@@ -1,14 +1,2 @@
-// src/mocks/handlers
-import { http, HttpResponse } from 'msw'
-
-export const handlers = [
-  // Intercept "GET https://example.com/user" requests...
-  http.get('/user', () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-      firstName: 'John',
-      lastName: 'Maverick',
-    })
-  }),
-]
+import { board } from './api'
+export const handlers = [board]
